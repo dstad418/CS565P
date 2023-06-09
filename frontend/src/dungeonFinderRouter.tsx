4ace4bbefb530017fd431cd04810b1cec55dd4d7
Dylan Herrigstad <dstad41@hotmail.com>
2023-06-09 18:47:42 -0400

    Router and Module section looking good using React.FC trick. Working on Importing to Campaign page

--- frontend/src/dungeonFinderRouter.tsx
+++ frontend/src/dungeonFinderRouter.tsx
@@ -7,7 +7,7 @@ import { UpdateProfile } from "@/Components/UpdateProfile.tsx";
 import { Campaign } from "@/Components/Campaigns.tsx";
 import { ProtectedRoute } from "@/Components/ProtectedRoute.tsx";
 import { useAuth } from "@/Services/Auth.tsx";
-import { Link, Route, Routes } from "react-router-dom";
+import { Link, Route, Routes, useParams } from "react-router-dom";
 
 export function DungeonFinderRouter() {
 	const auth = useAuth();
@@ -18,7 +18,8 @@ export function DungeonFinderRouter() {
 				<div className="navbar justify-center">
 					<div className={"navbar-center lg:flex"}>
 						<p><strong>Welcome to the Dungeon Finder! Please login to search for local games in your area!
-							<br />Good luck, and may all  your rolls be 20's!</strong></p>
+							<br />Good luck, and may all  your rolls be 20's!</strong>
+						</p>
 						<ul className={"ul-list-parent"}>
 							<li className={"nav-list"}><button className={"navbar-button"}><Link to="/">Home</Link></button></li>
 							{auth?.token != null ? (
@@ -32,8 +33,8 @@ export function DungeonFinderRouter() {
 									<li className={"nav-list"}><button className={"navbar-button"}><Link to="/login"> Login</Link></button></li>
 									<li className={"nav-list"}><button className={"navbar-button"}><Link to="/create"> Create Account</Link></button></li>
 								</>
-							)}</ul>
-
+							)}
+						</ul>
 					</div>
 				</div>
 			</nav>
@@ -42,7 +43,7 @@ export function DungeonFinderRouter() {
 				<Route path="/" element={<Home />} />
 				<Route path="/messages" element={<ProtectedRoute><MessageHistory/></ProtectedRoute>} />
 				<Route path="/updateProfile" element={<ProtectedRoute><UpdateProfile/></ProtectedRoute>} />
-				<Route path="/campaign/*" element={<ProtectedRoute><Campaign></ProtectedRoute>} />
+				<Route path="/campaign/*:dataToPass" element={<ProtectedRoute><Campaign /></ProtectedRoute>} />
 				<Route path="/campaign" element={<Home />} />
 				<Route path="/create" element={<CreateProfile/>}/>
 				<Route path="/login" element={<Login />} />
